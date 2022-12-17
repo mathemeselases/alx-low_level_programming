53fe379be7ae50c7daa9283119396a5e93b201df
root <mathemeselasesk@gmail.com>
2022-12-17 08:51:41 -0800

    5 print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * * main - prints out a string via printf
- * * Description: Program that prints out the string s followed by a new line
- * * Return: int 0 when done and no errors
- * */
+ * main - prints out a string via printf
+ * Description: Program that prints out the string s followed by a new line
+ * Return: int 0 when done and no errors
+ *
+ */
 
 int main(void)
 {
