539a24fec1e29bd48e6dd78d5ad4801687a0bff9
root <mathemeselasesk@gmail.com>
2022-12-20 06:31:06 -0800

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 	char l_letter;
-
+	
 	for (l_letter = 97; l_letter <= 122; l_letter++)
-	{
-		putchar(l_letter);
-		putchar('\n');
-		return (0);
-
+        {
+	     putchar(l_letter);
+	     putchar('\n');
 	}
+	return (0);
+}
