d50ec6c36d567b2f8a5b2e8ea2fbaedd3403eea2
root <mathemeselasesk@gmail.com>
2022-12-26 11:00:03 -0800

    alphabet 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lower case alphabet
+ *
+ * Return: alphabet lower case
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
