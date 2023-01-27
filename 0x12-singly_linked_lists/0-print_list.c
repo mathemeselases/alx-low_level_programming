d503771f38b988162f7ee09fedf60089631f8a91
mathemeselases <mathemeselasesk@gmail.com>
2023-01-26 18:28:35 -0800

    that prints all the elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: constant pointer type list_
+ * Return: the numbers of nodes
+ **/
+
+size_t print_list(const list_t *h)
+
+{
+size_t nodes;
+nodes = 0;
+while (h)
+{
+if (h->str == NULL)
+printf("[%d] %s\n", 0, "(nil)");
+else
+printf("[%d] %s\n", h->len, h->str);
+nodes++;
+h = h->next;
+}
+return (nodes);
+}
