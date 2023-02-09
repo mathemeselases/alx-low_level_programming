1e7ce2ffbc1a058ca13fe42a29f82dc5ef7c024d
mathemeselases <mathemeselasesk@gmail.com>
2023-02-08 22:14:46 -0800

    returns the value of a bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_bit - return one bit
+ * @n: the number
+ * @index: the position to read the bit
+ * Description: return 1 bit
+ * Return: the value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bit;
+
+	if (index > 64)
+		return (-1);
+
+	bit = n >> index;
+
+	return (bit & 0x1);
+}
