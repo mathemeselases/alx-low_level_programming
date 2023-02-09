f70218c645e72a192826a4df702d52def4c9e4bb
mathemeselases <mathemeselasesk@gmail.com>
2023-02-09 00:49:12 -0800

    Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: double pointer to the head of the list
+ *
+ * Return: the head nodes data (n)
+ */
+int pop_listint(listint_t **head)
+{
+	int n = 0;
+	listint_t *ptraux;
+	/* task condition */
+	if (*head == NULL)
+		return (0);
+	n = (*head)->n;
+	ptraux = (*head)->next;
+	free(*head);
+	*head = ptraux;
+	return (n);
+}
